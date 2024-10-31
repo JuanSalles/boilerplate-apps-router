97f56153e3da16db9df110b7669de84b0f9b13e3
Juan Salles <juanprazeres@gmail.com>
2024-10-31 09:30:46 -0300

    foo

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -1,18 +1,11 @@
 import { render, screen } from '@testing-library/react'
-
-import Main from '.'
+import Main from './index'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    //Renderiza o componente
-    const { container } = render(<Main />)
-    //Busca o elemento
-    //Verifica se o elemento está na tela
+    render(<Main />)
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
-
-    //gerar snapshot
-    expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 const Main = () => (
   <main>
-    <h1>React Avançadfedgterytrtyrtyrtdfgo</h1>
+    <h1>React Avançadfe</h1>
   </main>
 )
 
